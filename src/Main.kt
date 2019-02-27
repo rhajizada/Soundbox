426d8cdc5cc8498d0c74493b44455a5b9fc6d9e0
rhajizada <vice.genius@gmail.com>
2019-02-26 21:10:22 -0500

    Figured out how to access the values inside forms

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,29 @@
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLInputElement
 import kotlin.browser.document
 
 
 fun main(args: Array<String>) {
     println("Hello")
+    val main_card = document.getElementById("main_card") as HTMLDivElement
+    val spotify_input = document.getElementById("spotify_input") as HTMLInputElement
+    val apple_input = document.getElementById("apple_input") as HTMLInputElement
+    val tidal_input = document.getElementById("tidal_input") as HTMLInputElement
+    //tidal_input!!.placeholder = "I changed this using kotlin"
+
+    val submitBtn = document.createElement("button") as HTMLButtonElement
+    submitBtn.className = "btn btn-primary"
+    submitBtn.type = "submit"
+    submitBtn.innerText = "Submit"
+    main_card.appendChild(submitBtn)
+    fun printInputs(){
+        println("Spotify: ${spotify_input.value}")
+        println("Apple Music: ${apple_input.value}")
+        println("Tidal: ${tidal_input.value}")
+    }
+
+    submitBtn.addEventListener("click", { printInputs()})
 
 //    val x = "Generated using kotlin"
 //    var div = document.createElement("div")
