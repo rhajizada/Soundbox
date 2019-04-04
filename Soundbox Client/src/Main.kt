2754d7bd2e90b59c27f03eeba127da6c9cc6634f
rhajizada <vice.genius@gmail.com>
2019-04-04 19:55:41 -0400

    Fixed issue with git

---
+++ Soundbox Client/src/Main.kt
@@ -0,0 +1,64 @@
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLInputElement
+import kotlin.browser.*
+
+external class XMLHttpRequest
+var xhttp :dynamic= XMLHttpRequest(); // Adding native JavaScript library for http calls
+val main_card = document.getElementById("main_card") as HTMLDivElement
+val spotify_input = document.getElementById("spotify_input") as HTMLInputElement
+val apple_input = document.getElementById("apple_input") as HTMLInputElement
+val tidal_input = document.getElementById("tidal_input") as HTMLInputElement
+
+fun main(args: Array<String>) {
+
+    //tidal_input!!.placeholder = "I changed this using kotlin"
+    val submitBtn = document.createElement("button") as HTMLButtonElement
+    submitBtn.className = "btn btn-primary"
+    submitBtn.type = "submit"
+    submitBtn.innerText = "Submit"
+
+    var requestBtn = document.createElement("button") as HTMLButtonElement
+    requestBtn.className = "btn btn-primary"
+    requestBtn.type = "submit"
+    requestBtn.innerText = "Request sample"
+
+    main_card.appendChild(submitBtn)
+    main_card.appendChild(requestBtn)
+    submitBtn.addEventListener("click", { printInputs()})
+    requestBtn.addEventListener("click", {getAsync("http://0.0.0.0:8080/html-dsl"){response -> print(response)}})
+}
+
+fun printInputs(){
+    println("Spotify: ${spotify_input.value}")
+    println("Apple Music: ${apple_input.value}")
+    println("Tidal: ${tidal_input.value}")
+}
+
+fun getRequest(url: String): String{
+    var response: String = " "
+    xhttp.open("GET", url, true);
+    xhttp.onreadystatechange=fun(){
+        println(xhttp.readyState)
+        println(xhttp.status)
+        response =  xhttp.responseText as String
+    }
+    xhttp.send();
+    return response
+}
+
+private fun getAsync(url: String, callback: (String) -> Unit) {
+    val xmlHttp: dynamic = XMLHttpRequest()
+    if(xmlHttp) {
+        xmlHttp.open("GET",url) // Allows us easily bypass CORS
+        xmlHttp.withCredentials = true
+        xmlHttp.onload = {
+            if (xmlHttp.readyState == 4.toShort() && xmlHttp.status == 200.toShort()) {
+                callback.invoke(xmlHttp.responseText)
+            }
+        }
+        xmlHttp.send()
+    }
+
+}
+
